f65fe1dd16afa169c3701909bfbbd1776f886513
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2025-08-27 12:05:33 -0400

    copying from old folder

---
+++ Sample.cpp
@@ -0,0 +1,26 @@
+//@author: Shatokhin Mikhail
+//@date: mm/dd/yyyy
+//@purpose: 
+
+//include the iostream directive
+#include<iostream>
+
+//using the standart namespace
+using namespace std;
+
+// define the main function
+int main(){
+    
+    //initialize the variables
+
+    //comments 4use list:
+    //using std input to record a variable
+    //using std output to desplay variable/messege
+    //
+    //
+    //
+
+
+    //return 0 for good ethics of it all.
+    return 0;
+}
---
+++ week1/lab_1/product_of_two_numbers.cpp
@@ -0,0 +1,25 @@
+//@author: Shatokhin Mikhail
+//@date: mm/dd/yyyy
+//@purpose: 
+
+//include the iostream directive
+#include<iostream>
+
+//using the standart namespace
+using namespace std;
+
+// define the main function
+int main(){
+    
+    //initialize the variables
+    int num1 = 5;
+    int num2 = 10;
+    int product = num1*num2;
+
+    //using std output to desplay variable/messege
+    cout << product << endl;
+
+
+    //return 0 for good ethics of it all.
+    return 0;
+}
